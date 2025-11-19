6a9d8d22291d4ebdc23e424a8e1655dc2fd11400
Willem <91901507+wverbuyst@users.noreply.github.com>
2025-11-19 16:16:30 +0100

    Allow for puzzle selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+import { useState, type ReactElement } from "react";
 import { BluePuzzle } from "./components/Puzzles/BluePuzzle";
 import { GreenPuzzle } from "./components/Puzzles/GreenPuzzle";
 import { OrangePuzzle } from "./components/Puzzles/OrangePuzzle";
@@ -6,14 +8,37 @@ import { RedPuzzle } from "./components/Puzzles/RedPuzzle";
 import { YellowPuzzle } from "./components/Puzzles/YellowPuzzle";
 
 function App() {
+  const colors = [
+    "blue",
+    "green",
+    "orange",
+    "yellow",
+    "red",
+    "purple",
+  ] as const;
+  const [color, setColor] = useState<(typeof colors)[number]>("blue");
+  const Puzzles: Record<(typeof colors)[number], ReactElement> = {
+    blue: <BluePuzzle />,
+    green: <GreenPuzzle />,
+    orange: <OrangePuzzle />,
+    yellow: <YellowPuzzle />,
+    red: <RedPuzzle />,
+    purple: <PurplePuzzle />,
+  };
+
   return (
-    <div className="w-screen flex flex-col items-center p-20 gap-20">
-      <BluePuzzle />
-      <GreenPuzzle />
-      <OrangePuzzle />
-      <YellowPuzzle />
-      <RedPuzzle />
-      <PurplePuzzle />
+    <div className="w-screen flex flex-col items-center p-20 gap-14">
+      <div className="flex gap-2">
+        {colors.map((color) => (
+          <button
+            key={color}
+            type="button"
+            className={clsx("w-5 h-5", `bg-${color}-400`)}
+            onClick={() => setColor(color)}
+          />
+        ))}
+      </div>
+      {Puzzles[color]}
     </div>
   );
 }
