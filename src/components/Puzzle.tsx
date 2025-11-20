082206ac94b78b9d0665ccf843e73143e7ea8832
Willem <91901507+wverbuyst@users.noreply.github.com>
2025-11-20 06:11:20 +0100

    Have pieces underneath to keep it dynamic

--- src/components/Puzzle.tsx
+++ src/components/Puzzle.tsx
@@ -19,56 +19,35 @@ export function Puzzle({ puzzleConfig }: { puzzleConfig: PuzzleConfig }) {
 
   return (
     <div className="flex flex-col items-center gap-14">
-      <div className="flex gap-14">
-        {shuffledPieces.slice(0, 2).map((piece, index) => (
-          <Piece
-            key={`${puzzleConfig.color}-top-${index}`}
-            color={puzzleConfig.color}
-            piece={piece}
-          />
-        ))}
-      </div>
-      <div className="flex gap-14 items-center">
-        <Piece
-          key={`${puzzleConfig.color}-top-2`}
-          color={puzzleConfig.color}
-          piece={shuffledPieces[2]}
-        />
-        <div
-          style={
-            {
-              "--rows": puzzleConfig.frame.length,
-              "--cols": puzzleConfig.frame[0].length,
-              display: "grid",
-              gridTemplateRows: "repeat(var(--rows), 20px)",
-              gridTemplateColumns: "repeat(var(--cols), 20px)",
-            } as React.CSSProperties
-          }
-        >
-          {Array.from({ length: puzzleConfig.frame.length }).map((_, r) =>
-            Array.from({ length: puzzleConfig.frame[0].length }).map((_, c) => {
-              const i = r * puzzleConfig.frame[0].length + c;
-              return (
-                <div
-                  key={`${r}-${c}`}
-                  className={clsx(
-                    "w-5 h-5",
-                    frameAsUint8Array[i] ? puzzleConfig.color : "bg-gray-200",
-                  )}
-                />
-              );
-            }),
-          )}
-        </div>
-        <Piece
-          key={`${puzzleConfig.color}-top-3`}
-          color={puzzleConfig.color}
-          piece={shuffledPieces[3]}
-        />
+      <div
+        style={
+          {
+            "--rows": puzzleConfig.frame.length,
+            "--cols": puzzleConfig.frame[0].length,
+            display: "grid",
+            gridTemplateRows: "repeat(var(--rows), 20px)",
+            gridTemplateColumns: "repeat(var(--cols), 20px)",
+          } as React.CSSProperties
+        }
+      >
+        {Array.from({ length: puzzleConfig.frame.length }).map((_, r) =>
+          Array.from({ length: puzzleConfig.frame[0].length }).map((_, c) => {
+            const i = r * puzzleConfig.frame[0].length + c;
+            return (
+              <div
+                key={`${r}-${c}`}
+                className={clsx(
+                  "w-5 h-5",
+                  frameAsUint8Array[i] ? puzzleConfig.color : "bg-gray-200",
+                )}
+              />
+            );
+          }),
+        )}
       </div>
 
-      <div className="flex gap-14">
-        {shuffledPieces.slice(4, 6).map((piece, index) => (
+      <div className="flex gap-2">
+        {shuffledPieces.map((piece, index) => (
           <Piece
             key={`${puzzleConfig.color}-${index}`}
             color={puzzleConfig.color}
